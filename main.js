e7a329a5f1ddd8a23ab88140e4625d98e472c587
Emmanuel Santiago <biosinx@gmail.com>
2021-01-26 19:03:55 -0500

    operadores_asignacion_26_01_2021

---
+++ main.js
@@ -0,0 +1,67 @@
+//Operadores Asignacion
+
+let x = 10;
+let y = 2;
+
+console.log('Operadores Asignacion',x)
+
+//Operadores adiccion
+let a = 10;
+let b = 2;
+//Abreviacion
+a += b;
+//Significado
+//x = x + y;
+
+console.log('Operadores adiccion',b);
+
+//Operadores sustraccion
+let c = 10;
+let d = 2;
+//Abreviacion
+c -= d;
+//Significado
+//x = x - y;
+
+console.log('Operadores sustraccion',c);
+
+//Operadores multiplicacion
+let e = 10;
+let f = 2;
+//Abreviacion
+e *= f;
+//Significado
+//x = x * y;
+
+console.log('Operadores multiplicacion',e);
+
+//Operadores division
+let g = 10;
+let h = 2;
+//Abreviacion
+g /= h;
+//Significado
+//x = x / y;
+
+console.log('Operadores division',g);
+
+//Operadores resto
+let j = 10;
+let k = 2;
+//Abreviado
+j %= k;
+//significado
+//x = x % y;
+
+console.log('Operadores resto',j);
+
+//Operador exponecial
+let m = 10;
+let n = 2;
+//Abreviado
+m **= n;
+//Significado
+//x = x ** y;
+
+console.log('Operador exponecial',m)
+
